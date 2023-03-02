e5763f8d5058a89ab44fe813c58792b457a7dedd
heewon0318 <heewonyoo0318@gmail.com>
2023-03-02 21:00:12 +0900

    1

---
+++ 001_sum_of_num.py
@@ -0,0 +1,15 @@
+print ("숫자의 개수를 입력하시오")
+n = int(input())
+print ("0~9 숫자를 위에 입력한 수만큼 입력하시오")
+numbers = list(input())
+sum = 0
+
+if n == len(numbers):
+    for i in numbers:
+        sum = sum + int(i)
+    print(sum)
+else:
+    print("입력한 숫자의 개수가 설정값과 다릅니다.")
+
+
+
